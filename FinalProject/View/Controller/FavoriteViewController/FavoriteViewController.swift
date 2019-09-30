0ea64859dde31978fe9170e02f16407cb6e5b2f5
PCI0010 <hung.pham@asiantech.vn>
2019-09-30 09:15:17 +0700

    Update tabbar for Final_Project

---
+++ FinalProject/View/Controller/FavoriteViewController/FavoriteViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FavoriteViewController.swift
+//  FinalProject
+//
+//  Created by PCI0010 on 9/30/19.
+//  Copyright © 2019 Asiantech. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+    }
+
+    func configUI() {
+        title = "FAVORITE"
+        navigationController?.navigationBar.backgroundColor = .red
+    }
+}
